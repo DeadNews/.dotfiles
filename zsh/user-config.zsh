9e7d3e432bc63657de994c05afc4839bc526226d
DeadNews <uhjnnn@gmail.com>
2022-11-14 22:08:52 +0600

    Update zsh

--- zsh/user-config.zsh
+++ zsh/user-config.zsh
@@ -1,7 +1,23 @@
+## Alias section
+alias ls='exa'
+
 ## Funcs section
 cheat() {
-    curl "https://cheat.sh/$1"
+  curl "https://cheat.sh/$1"
 }
 ipinfo() {
-    curl "https://ipinfo.io/$1"
+  curl "https://ipinfo.io/$1"
 }
+
+## Plugins section
+if [[ -e /usr/share/zsh/plugins/fzf-tab-git/fzf-tab.plugin.zsh ]]; then
+  source /usr/share/zsh/plugins/fzf-tab-git/fzf-tab.plugin.zsh
+  # set colors
+  export FZF_DEFAULT_OPTS='--color=gutter:-1,bg+:#4f5666'
+fi
+if [[ -e /usr/share/zsh/plugins/history-search-multi-word/history-search-multi-word.plugin.zsh ]]; then
+  source /usr/share/zsh/plugins/history-search-multi-word/history-search-multi-word.plugin.zsh
+  # set colors
+  zstyle "history-search-multi-word" highlight-color "bg=#4f5666,bold"
+  zstyle ":plugin:history-search-multi-word" active "standout"
+fi
